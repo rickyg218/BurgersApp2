7bffe1275a4a3b16d5e2f2abe92fcbeb649c19e0
Enrique Garcia <rickyg218@gmail.com>
2020-07-20 22:18:05 -0700

    didn't have time to style

---
+++ BurgerApp/db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burger (name) VALUES ('bacon and cheese');
+INSERT INTO burger (name) VALUES ('Bobbi lovy burger');
+INSERT INTO burger (name,devoured) VALUES ('Baconator', true);
+INSERT INTO burger (name, devoured) VALUES ('PineApple Delight', true);
+INSERT INTO burger (name, devoured) VALUES ('Chewy qaurter pounder', true);
+INSERT INTO burger (name) VALUES ('Teriyaki burger');
